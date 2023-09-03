1244bc6576e748088f24a47aed8810fdefe87836
Lev <lev.lvi2@gmail.com>
2023-09-03 20:50:40 +0300

    Creation base of cities and recording in file

--- src/main/java/org/example/InterfaceGame.java
+++ src/main/java/org/example/InterfaceGame.java
@@ -2,75 +2,115 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class InterfaceGame {
+    private static JFrame welcomeFrame;
     private static JFrame gameFrame;
-    private static JTextField inputField;
-    private static JButton goButon;
-    private static String computerСourse;
-    private static String userCourse;
-    private static JLabel coputerLable;
-    private static JLabel goCourse;
-    private static JPanel gameUp;
-    private static JPanel gameDown;
-   private static LogicGame logicGame;
-    public static void welcomMetods() {
 
-        final JFrame welcomeFrame = new JFrame("Вітаємо");
-        JPanel jPanel = new JPanel();
-        jPanel.setLayout(new FlowLayout());
+    public static void welcomeMethods() {
+        welcomeFrame = new JFrame("Вітаємо");
         welcomeFrame.setSize(400, 100);
-        welcomeFrame.setDefaultCloseOperation(3);
-        JLabel labelWelcom = new JLabel("Вітаємо вас у грі дитинства і всіх розумників!");
-        labelWelcom.setHorizontalAlignment(2);
-        JButton startButton = new JButton("ОК!");
-        startButton.addActionListener(new ActionListener() {
+        welcomeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Вітаємо вас у грі дитинства і всіх розумників!");
+        label.setFont(new Font("Arial", Font.PLAIN, 16));
+        label.setHorizontalAlignment(JLabel.CENTER);
+        welcomeFrame.add(label, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton button = new JButton("Ок, Старт!");
+        button.setPreferredSize(new Dimension(100, 30));
+        button.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                welcomeFrame.setVisible(false);
-                InterfaceGame.gameFrame.setVisible(true);
+                welcomeFrame.dispose(); // Закриття першого вікна
+                createGameFrame();
             }
         });
-        jPanel.add(labelWelcom);
-        jPanel.add(startButton);
-        welcomeFrame.add(jPanel);
-        welcomeFrame.setLocationRelativeTo((Component)null);
-        welcomeFrame.setVisible(true);
+        buttonPanel.add(button);
+
+        welcomeFrame.add(buttonPanel, BorderLayout.SOUTH);
+
+        location(welcomeFrame); // розположення вікна на екрані
     }
+
     public static void createGameFrame() {
         gameFrame = new JFrame("Cities Game");
-        gameUp = new JPanel();
-        gameDown = new JPanel();
-        gameUp.setLayout(new FlowLayout());
-        gameDown.setLayout(new FlowLayout());
-        gameFrame.setSize(400, 200);
-        gameFrame.setDefaultCloseOperation(3);
-        gameFrame.setLayout(new BorderLayout());
-        inputField = new JTextField();
-        inputField.setPreferredSize(new Dimension(100, inputField.getPreferredSize().height));
-        inputField.setHorizontalAlignment(2);
-        goCourse = new JLabel("Введіть назву міста");
-        goButon = new JButton("Зробити хід!");
-        goButon.setHorizontalAlignment(2);
-        coputerLable = new JLabel();
-        logicGame=new LogicGame();
-        goButon.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                InterfaceGame.userCourse = InterfaceGame.inputField.getText();
-                logicGame.setUserCourse(userCourse);
-                InterfaceGame.inputField.setText("");
-                InterfaceGame.computerСourse = logicGame.getComputerCrse();//Вставиш значення від комп'ютера
-                InterfaceGame.coputerLable.setText("Комп'ютер: " + InterfaceGame.computerСourse);
-            }
-        });
-        gameUp.add(inputField);
-        gameUp.add(goCourse);
-        gameDown.add(goButon);
-        gameDown.add(coputerLable);
-        gameFrame.add(gameUp, "North");
-        gameFrame.add(gameDown, "South");
-        gameFrame.setLocationRelativeTo((Component)null);
-        gameFrame.setVisible(false);
+        gameFrame.setSize(400, 500);
+        gameFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Верхня панель
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
+
+        JLabel goCourse = new JLabel("Введіть назву міста");
+        goCourse.setFont(new Font("Arial", Font.PLAIN, 16));
+        goCourse.setAlignmentX(Component.CENTER_ALIGNMENT); // Вирівнювання по центру
+        topPanel.add(goCourse);
+
+        JTextField inputField = new JTextField();
+        inputField.setPreferredSize(new Dimension(100, 30));
+        inputField.setHorizontalAlignment(JTextField.CENTER);
+        inputField.setAlignmentX(Component.CENTER_ALIGNMENT); // Вирівнювання по центру
+        topPanel.add(inputField);
+
+        JLabel score = new JLabel("Гравець: " + 10 + "/ Комп'ютер: " + 10);
+        score.setFont(new Font("Arial", Font.PLAIN, 16));
+        score.setAlignmentX(Component.CENTER_ALIGNMENT); // Вирівнювання по центру
+        topPanel.add(score);
+
+        // Ліва панель зі списком слів (поповнюється)
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+
+        JTextArea wordListLeft = new JTextArea("Слово A\nСлово B\nСлово C\nСлово A\nСлово B\nСлово "); // Тут можна поповнювати список слів
+        wordListLeft.setFont(new Font("Arial", Font.PLAIN, 16));
+        wordListLeft.setEditable(false); // Заборона редагування
+        wordListLeft.setAlignmentX(Component.CENTER_ALIGNMENT); // Вирівнювання по центру
+        leftPanel.add(wordListLeft);
+
+        // Права панель зі списком слів (такий самий, як в лівій панелі)
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+
+        JTextArea wordListRight = new JTextArea("Слово A\nСлово B\nСлово C\nСлово A\nСлово B\nСлово C"); // Тут можна поповнювати список слів
+        wordListRight.setFont(new Font("Arial", Font.PLAIN, 16));
+        wordListRight.setEditable(false); // Заборона редагування
+        wordListRight.setAlignmentX(Component.CENTER_ALIGNMENT); // Вирівнювання по центру
+        rightPanel.add(wordListRight);
+
+        // Нижня панель з полем для виводу інформації і кнопкою
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.Y_AXIS));
+
+        JTextField infoField = new JTextField("Комп'ютер: "); // Тут можна виводити інформацію
+        infoField.setFont(new Font("Arial", Font.PLAIN, 16));
+        infoField.setEditable(false); // Заборона редагування
+        infoField.setAlignmentX(Component.CENTER_ALIGNMENT); // Вирівнювання по центру
+        bottomPanel.add(infoField);
+
+        JButton button = new JButton("Натисніть мене");
+        button.setAlignmentX(Component.CENTER_ALIGNMENT); // Вирівнювання по центру
+        bottomPanel.add(button);
+
+        // Додавання всіх панелей до головного вікна
+        gameFrame.add(topPanel, BorderLayout.NORTH);
+        gameFrame.add(leftPanel, BorderLayout.WEST);
+        gameFrame.add(rightPanel, BorderLayout.EAST);
+        gameFrame.add(bottomPanel, BorderLayout.SOUTH);
+
+        location(gameFrame); // розположення вікна на екрані
     }
+
+
+    private static void location(JFrame frame){
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((screenSize.getWidth() - frame.getWidth()) / 2);
+        int y = (int) ((screenSize.getHeight() - frame.getHeight()) / 2);
+        frame.setLocation(x, y);
+
+        frame.setVisible(true);
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,10 +11,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-        LoadingBase loadingBase = new LoadingBase();
-        loadingBase.LoadList();
-        InterfaceGame.welcomMetods();
-        InterfaceGame.createGameFrame();
+
+        InterfaceGame.welcomeMethods();
+
     }
 
 }
