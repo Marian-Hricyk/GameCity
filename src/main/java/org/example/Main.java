d5deca5bd0714aacf95398071535a8443a262fa2
Lev <lev.lvi2@gmail.com>
2023-09-05 19:17:14 +0300

    new version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,23 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        InterfaceGame.createGameFrame(); // ініціалізуемо ігровий інтерфейс
-        InterfaceGame.welcomeMethods();   // Запускаємо экран
-    }
-}
+import java.io.File;
 
+public class Main {
+
+
+    public static void main(String[] args) {
+        //перевірка на наявність бази(вона повина завантажитись лише раз)
+        String fileName = "cities_list.txt";
+        File rootFolder = new File(System.getProperty("user.dir"));
+        File fileToCheck = new File(rootFolder, fileName);
+        if (fileToCheck.exists()) {
+            System.out.println("Файл cities_list.txt вже існує в кореневій папці.");
+        } else {
+            LoadingBase.LoadList();
+        }
+        //завантажуємо ігровий інтерфейс
+        InterfaceGame.welcomeMethods();
+
+    }
+
+}
